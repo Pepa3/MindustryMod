5748e30e186de37d17cbf71071576da89822f737
Pepa3 <svobodajosef117@gmail.com>
2022-05-06 21:48:51 +0200

    update v0.1.3, smelter

--- src/mod/MainMod.java
+++ src/mod/MainMod.java
@@ -1,26 +1,33 @@
 package mod;
 
 import arc.*;
+import arc.graphics.*;
 import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
+import mindustry.type.*;
 import mindustry.ui.dialogs.*;
+import mindustry.world.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.production.*;
+import static mindustry.type.ItemStack.*;
 
 public class MainMod extends Mod{
+	public static Item frogMetal, frogRaw;
+	public static Block oreFrog;
+	public static GenericCrafter frogSmelter;
 
     public MainMod(){
-        Log.info("Loaded constructor.");
+        Log.info("[AT]Loaded constructor.");
 
-        //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
+        	
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("allthings-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
@@ -30,7 +37,44 @@ public class MainMod extends Mod{
 
     @Override
     public void loadContent(){
-        Log.info("Loading content.");
+        Log.info("[AT]Loading content.");
+        loadItems();
+        loadBlocks();
+        loadUnits();
     }
-
+    
+    private void loadItems() {
+    	frogRaw = new Item("frog-raw", Color.valueOf("ebeef5")){{
+    		alwaysUnlocked = true;
+            cost = 1.5f;
+            hardness = 1;
+        }};
+    	frogMetal = new Item("frog-metal", Color.valueOf("ebefe5")){{
+    		
+            cost = 1.5f;
+            hardness = 1;
+        }};
+    }
+    private void loadBlocks() {
+    	oreFrog = new OreBlock(frogRaw){{
+    		
+            oreDefault = true;
+            oreThreshold = 0.882f;
+            oreScale = 25.380953f;
+        }};
+        
+        frogSmelter = new GenericCrafter("frog-smelter"){{
+            requirements(Category.crafting, with(frogRaw, 50, Items.copper, 30));
+            
+            craftEffect = Fx.pulverizeMedium;
+            outputItem = new ItemStack(frogMetal, 1);
+            craftTime = 120f;
+            size = 2;
+            hasItems = true;
+            
+            consumes.power(0.25f);
+            consumes.item(frogRaw, 2);
+        }};
+    }
+    private void loadUnits() {}
 }
